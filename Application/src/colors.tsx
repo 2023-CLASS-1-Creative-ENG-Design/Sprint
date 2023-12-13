7fcd724d9d95846a233b618ab9d5d71610bd2342
mini-777 <pho0570@gmail.com>
2023-12-13 10:46:24 +0900

    Remove unnecessary files and configurations

--- Application/src/colors.tsx
+++
@@ -1,8 +0,0 @@
-const colors = {
-  dark: '#2e383f',
-  white: '#ececeb',
-  water: '#07617d',
-  sun: '#f9a828'
-}
-
-export default colors
--- Application/src/types.tsx
+++
@@ -1,9 +0,0 @@
-import type {NativeStackScreenProps} from '@react-navigation/native-stack'
-
-export type RootStackParamList = {
-  Scan: undefined
-  Controller: {deviceName: string}
-}
-
-export type ScanProps = NativeStackScreenProps<RootStackParamList, 'Scan'>
-export type ControllerProps = NativeStackScreenProps<RootStackParamList, 'Controller'>
